23fac91e5587f7e00e0613e75f1250cb6b9f4308
Jingyuango <154586885+Jingyuango@users.noreply.github.com>
2025-03-18 22:44:30 +0800

    Add files via upload

---
+++ file_split_merge.py
@@ -0,0 +1,19 @@
+from filesplit.merge import Merge
+from filesplit.split import Split
+import os, glob
+
+path = os.getcwd().replace("\\", "/")
+
+def fsplit_fun():
+    for i in glob.glob("*.pkl"):
+        os.mkdir(i.strip(".pkl"))
+        split = Split(i, i.strip(".pkl"))
+        split.bysize(size = 1024*1000*20)
+        
+def fmerge_fun(files):
+    for i in files:
+        merge = Merge(path+"/"+i, path, i+"_h.h5")
+        merge.merge()
+
+fsplit_fun()
+#fmerge_fun(["model1"])
